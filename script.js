7bcfb6a703a7d587b1f15695d583e4ef933c4a0a
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-10-07 22:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+let newtodo = document.getElementById('newTodoInput');
+let ol = document.getElementById('todoList');
+let addbtn = document.getElementById('addTodoBtn');
+let arrList = [];
+
+addbtn.addEventListener("click",()=>{
+	if (newtodo.value.trim() !== "") {
+		ol.innerText ="";
+		arrList.push(newtodo.value);
+		newtodo.value="";
+		arrList.forEach(item=>{
+			let li = document.createElement('li');
+			li.innerText = `${item}`
+			ol.appendChild(li);
+		})
+	}
+})
 
